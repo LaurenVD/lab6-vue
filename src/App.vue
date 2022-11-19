1b332c280337c8a55afadf26f71980a0fb7759d9
LaurenV <laurenvdl45@gmail.com>
2022-11-19 10:36:18 +0100

    build: setup files start from class

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+ // import VideoPlayer.vue
+import VideoPlayer from './components/VideoPlayer.vue'
+  // import VideoDetails.vue
+import VideoDetails from './components/VideoDetails.vue'
+</script>
+
+<template>
+  <div class="app" >
+    <VideoPlayer />
+    <VideoDetails />
+  </div>
+</template>
+
+<style scoped>
+.app {
+  display: grid;
+  grid-template-columns: 8fr 4fr;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+</style>
---
+++ src/components/VideoDetails.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import { ref, onMounted } from 'vue';
+
+    let username = ref("");
+    let description = ref("");
+
+    // onMounted
+    onMounted(() => {
+        const apiUrl = "https://app.fakejson.com/q/Tb9Kbh0b?token=3LVGJEYJ1USZTvt0w9VmZw"
+        fetch(apiUrl)
+            .then(res => res.json())
+            .then(data => {
+                username.value = data.videos[0].username;
+                description.value = data.videos[0].description;
+            })
+    })
+</script>
+
+<template>
+    <div>
+        <h2>{{ username }}</h2>
+        <p>{{ description }}</p>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        padding: 0 2rem;
+    }
+</style>
